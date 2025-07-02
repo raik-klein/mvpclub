7f6b607e0eb8e19b7511283c5bdf26a68e9a9cd4
raik-klein <33813177+raik-klein@users.noreply.github.com>
2025-07-02 14:32:10 +0200

    Sanitize player info output

--- players.php
+++ players.php
@@ -664,6 +664,7 @@ function mvpclub_render_player_info($attributes) {
     $placeholders = mvpclub_player_placeholders($player_id);
 
     $content = str_replace(array_keys($placeholders), array_values($placeholders), $code);
+    $content = force_balance_tags( wp_kses_post( $content ) );
     ob_start();
     echo '<div class="mvpclub-player-info" style="background:' . esc_attr($bg) . ';color:' . esc_attr($text) . ';padding:1em;">';
     echo $content;
